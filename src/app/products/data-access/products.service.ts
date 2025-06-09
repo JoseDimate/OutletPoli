28eab604446eb8456cd0d2874401ba954a0099b2
JoseDimate <jdimate@poligran.edu.co>
2025-06-09 11:50:40 -0500

    Precios

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -3,7 +3,7 @@ import { BaseHttpService } from '../../shared/data-access/base-http.service';
 import { Observable } from 'rxjs';
 import { Product } from '../../shared/interfaces/product.interface';
 
-const LIMIT = 20;
+const LIMIT = 4;
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService extends BaseHttpService {
